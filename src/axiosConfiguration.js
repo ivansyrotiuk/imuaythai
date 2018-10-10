b4b4d0be635c8628f06327add4a7ec0dab2b2bda
Ivan Syrotiuk <jbt873@motorolasolutions.com>
2018-10-10 09:54:09 +0200

    Travis

--- src/axiosConfiguration.js
+++ src/axiosConfiguration.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 export const configApiHost = () => {
-  if (process.env.NODE_ENV === "production")
-    axios.defaults.baseURL = "https://imuaythai-api.herokuapp.com/";
-  else if (process.env.NODE_ENV === "development")
-    axios.defaults.baseURL = "https://imuaythai-api-dev.herokuapp.com/";
-  else
-    axios.defaults.baseURL = "http://localhost:5000/";
+    if (process.env.NODE_ENV === "production") {
+        axios.defaults.baseURL = "https://imuaythai-api.herokuapp.com/";
+        axios.defaults.baseURL = process.env.API_HOST;
+    } else if (process.env.NODE_ENV === "development") {
+        axios.defaults.baseURL = "https://imuaythai-api-dev.herokuapp.com/";
+        axios.defaults.baseURL = process.env.API_HOST;
+    } else axios.defaults.baseURL = "http://localhost:5000/";
 };
 
 export const setAuthToken = token => {
-  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+    axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 };
