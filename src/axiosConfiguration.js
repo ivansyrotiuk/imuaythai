31ec671ed27ebe72f27fb2d9f6608584cf5ae1b3
Ivan Syrotiuk <jbt873@motorolasolutions.com>
2018-10-10 10:23:54 +0200

    Travis

--- src/axiosConfiguration.js
+++ src/axiosConfiguration.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const configApiHost = () => {
-    if (process.env.REACT_APP_HOST_ENV === "production") {
+    if (process.env.HOST_ENV === "production") {
         axios.defaults.baseURL = "https://imuaythai-api.herokuapp.com/";
-    } else if (process.env.REACT_APP_HOST_ENV === "development") {
+    } else if (process.env.HOST_ENV === "development") {
         axios.defaults.baseURL = "https://imuaythai-api-dev.herokuapp.com/";
     } else axios.defaults.baseURL = "http://localhost:5000/";
 };
